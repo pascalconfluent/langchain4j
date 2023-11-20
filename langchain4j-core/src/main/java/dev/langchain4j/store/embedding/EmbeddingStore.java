1c7530abad5bf9f0fe83d978a4f14c7b3ca55616
Pascal Vantrepote <pascal@confluent.io>
2023-11-20 12:37:27 -0500

    Adding addAll, Update and Delete for vector store

--- langchain4j-core/src/main/java/dev/langchain4j/store/embedding/EmbeddingStore.java
+++ langchain4j-core/src/main/java/dev/langchain4j/store/embedding/EmbeddingStore.java
@@ -37,6 +37,17 @@ public interface EmbeddingStore<Embedded> {
     String add(Embedding embedding, Embedded embedded);
 
     /**
+     * Adds a given embedding and the corresponding content that has been embedded to the store.
+     *
+     * @param id        The unique identifier for the embedding to be added.
+     * @param embedding The embedding to be added to the store.
+     * @param embedded  Original content that was embedded.
+     */
+    default void add(String id, Embedding embedding, Embedded embedded) {
+        throw new UnsupportedOperationException("Add not implemented");
+    }
+
+    /**
      * Adds multiple embeddings to the store.
      *
      * @param embeddings A list of embeddings to be added to the store.
@@ -52,6 +63,67 @@ public interface EmbeddingStore<Embedded> {
      * @return A list of auto-generated IDs associated with the added embeddings.
      */
     List<String> addAll(List<Embedding> embeddings, List<Embedded> embedded);
+
+    /**
+     * Adds multiple embeddings and their corresponding contents that have been embedded to the store.
+     *
+     * @param ids        A list of unique identifiers for the embeddings to be added.
+     * @param embeddings A list of embeddings to be added to the store.
+     * @param embedded   A list of original contents that were embedded.
+     */
+    default List<String> addAll(List<String> ids, List<Embedding> embeddings, List<Embedded> embedded) {
+        throw new UnsupportedOperationException("Add all not implemented");
+    }
+
+    /**
+     * Updates an embedding in the store.
+     *
+     * @param id        The unique identifier of the embedding to be updated.
+     * @param embedding The new embedding.
+     */
+    default void update(String id, Embedding embedding) {
+        throw new UnsupportedOperationException("Update not implemented");
+    }
+
+    /**
+     * Updates an embedding in the store.
+     *
+     * @param id        The unique identifier of the embedding to be updated.
+     * @param embedding The new embedding.
+     * @param embedded  The new content that has been embedded.
+     */
+    default void update(String id, Embedding embedding, Embedded embedded) {
+        throw new UnsupportedOperationException("Update not implemented");
+    }
+
+    /**
+     * Updates multiple embeddings in the store.
+     *
+     * @param ids        The unique identifiers of the embeddings to be updated.
+     * @param embeddings The new embeddings.
+     * @param embedded   The new contents that have been embedded.
+     */
+    default void updateAll(List<String> ids, List<Embedding> embeddings, List<Embedded> embedded) {
+        throw new UnsupportedOperationException("Update all not implemented");
+    }
+
+    /**
+     * Deletes an embedding from the store.
+     *
+     * @param id The unique identifier of the embedding to be deleted.
+     */
+    default void delete(String id) {
+        throw new UnsupportedOperationException("Delete not implemented");
+    }
+
+    /**
+     * Deletes multiple embeddings from the store.
+     *
+     * @param ids A list of unique identifiers of the embeddings to be deleted.
+     */
+    default void deleteAll(List<String> ids) {
+        throw new UnsupportedOperationException("Delete not implemented");
+    }
 
     /**
      * Finds the most relevant (closest in space) embeddings to the provided reference embedding.
